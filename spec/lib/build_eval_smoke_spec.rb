fe63378812ebede36bacbd0df0d1b07cba86ea2d
Ryan Davis <ryan.davis.dev@gmail.com>
2016-05-31 14:31:52 +1000

    Allow an error status to be received from TravisCI in integration tests

--- spec/lib/build_eval_smoke_spec.rb
+++ spec/lib/build_eval_smoke_spec.rb
@@ -7,7 +7,12 @@ describe BuildEval, "integrating with a real CI server", smoke: true do
   describe "the evaluated results from a build monitor for the server" do
 
     let(:tolerated_statuses) do
-      [ BuildEval::Result::Status::SUCCESS, BuildEval::Result::Status::UNKNOWN, BuildEval::Result::Status::FAILURE ]
+      [
+        BuildEval::Result::Status::SUCCESS,
+        BuildEval::Result::Status::UNKNOWN,
+        BuildEval::Result::Status::FAILURE,
+        BuildEval::Result::Status::ERROR
+      ]
     end
 
     subject { monitor.evaluate }
